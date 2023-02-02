11fc1280d511e7147adb15de5ff4c4a7935272aa
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-02 15:30:03 -0300

    Estrutura do projeto gerada

--- lista-tarefas/src/TodoList.jsx
+++ lista-tarefas/src/TodoList.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import './TodoList.css';
 
 function TodoList(){
-    return(<h1>Olá Mundo!</h1>)
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input 
+                type="text"
+                placeholder="Adiconar uma tarefa"
+                />
+                <button type="submit">Add</button>
+            </form>
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+
+                <div className="item completo">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default TodoList
