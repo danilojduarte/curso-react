5050b11e80881b01304f76504171814b320a2a42
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-07 00:54:43 -0300

    Implementando a lista

--- lista-tarefas/src/TodoList.jsx
+++ lista-tarefas/src/TodoList.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import './TodoList.css';
+import Icone from './assets/icon.webp';
 
-function TodoList(){
+function TodoList() {
 
-const [ lista, setLista ] = useState([]);
-const [ novoItem, setNovoItem ] = useState("");
+    const [lista, setLista] = useState([]);
+    const [novoItem, setNovoItem] = useState("");
 
-    function adicionaItem(form){
+    function adicionaItem(form) {
         form.preventDefault();
         if (!novoItem) {
             return;
         }
-        setLista([...lista, {text: novoItem, isCompleted:false}]);
+        setLista([...lista, { text: novoItem, isCompleted: false }]);
         setNovoItem("");
         document.getElementById('input-entrada').focus();
     }
@@ -20,26 +21,34 @@ const [ novoItem, setNovoItem ] = useState("");
         <div>
             <h1>Lista de Tarefas</h1>
             <form onSubmit={adcionaItem}>
-                <input 
-                id="input-entrada"
-                type="text"
-                value={novoItem}
-                onChange = {(e) => {setNovoItem(e.target.value)}}
-                placeholder="Adiconar uma tarefa"
+                <input
+                    id="input-entrada"
+                    type="text"
+                    value={novoItem}
+                    onChange={(e) => { setNovoItem(e.target.value) }}
+                    placeholder="Adiconar uma tarefa"
                 />
                 <button className="add" type="submit">Add</button>
             </form>
             <div className="listaTarefas">
-                <div className="item">
-                    <span>Tarefa de exemplo</span>
-                    <button className="del">Deletar</button>
-                </div>
+                <div>
+                    {
+                        lista.length < 1
+                            ?
+                            <img src={Icone} />
+                    :
+                            <div className="item">
+                                <span>Tarefa de exemplo</span>
+                                <button className="del">Deletar</button>
+                            </div>
+                    }
 
-                <div className="item completo">
-                    <span>Tarefa de exemplo</span>
-                    <button className="del">Deletar</button>
+                    <div className="item completo">
+                        <span>Tarefa de exemplo</span>
+                        <button className="del">Deletar</button>
+                    </div>
+                    <button className="deleteAll">Deletar Todas</button>
                 </div>
-                <button className="deleteAll">Deletar Todas</button>
             </div>
         </div>
     )
